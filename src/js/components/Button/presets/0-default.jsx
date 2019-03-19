f3829f8d405f4cbb44f5516a2dd80823e80a4560
Marcin Treder <marcin@uxpin.com>
2019-03-19 16:48:56 -0700

    changes for recording

--- src/js/components/Button/presets/0-default.jsx
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-named-as-default */
-import * as React from 'react';
-import Button from '../Button';
-
-export default <Button uxpId="button0" label="hello merge!" fill primary />;
